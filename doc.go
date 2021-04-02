3bbf76239ad1b203af6402e00dc5faf1d28c539c
蒋佳李 <1036226474@qq.com>
2021-04-02 17:29:56 +0800

    更新

---
+++ doc.go
@@ -0,0 +1,21 @@
+/*
+  package main
+
+  import (
+    log "github.com/jiangjiali/logrus"
+  )
+
+  func main() {
+    log.WithFields(log.Fields{
+      "animal": "walrus",
+      "number": 1,
+      "size":   10,
+    }).Info("A walrus appears")
+  }
+
+Output:
+  time="2015-09-07T08:48:33Z" level=info msg="A walrus appears" animal=walrus number=1 size=10
+
+For a full guide visit https://github.com/sirupsen/logrus
+*/
+package logrus
